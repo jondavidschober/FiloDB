4335654d6656c4fb85be18a9501fba3065b55a43
Jon David Schober <jondavid.schober@apple.com>
2025-01-13 15:42:46 -0800

    Invert condition

--- core/src/main/scala/filodb.core/memstore/TimeSeriesShard.scala
+++ core/src/main/scala/filodb.core/memstore/TimeSeriesShard.scala
@@ -1428,7 +1428,7 @@ class TimeSeriesShard(val ref: DatasetRef,
   }
 
   private def commitCheckpoint(ref: DatasetRef, shardNum: Int, flushGroup: FlushGroup): Future[Response] = {
-    if (storeConfig.flushWriteEnabled) {
+    if (!storeConfig.flushWriteEnabled) {
       logger.warn("Flush Writes are disabled. Skipping Flush")
       return Future.successful(NotApplied)
     }
